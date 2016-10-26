4cc6664dc63d9fb9196268532f3696f325e5d9c8
Anton Maminov <anton.maminov@gmail.com>
2016-10-26 15:33:28 +0300

    create users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,8 +1,9 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all Rumbl.User
+    users = Repo.all User
 
     render conn, "index.html", users: users
   end
@@ -10,7 +11,22 @@ defmodule Rumbl.UserController do
   def show(conn, %{ "id" => id }) do
     user_id = String.to_integer(id)
 
-    user = Repo.get(Rumbl.User, user_id)
+    user = Repo.get(User, user_id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :show, user.id))
   end
 end
