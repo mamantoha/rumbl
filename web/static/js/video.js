9f627d4d53d10037d13a975981ab0c056bd602a5
Anton Maminov <anton.maminov@gmail.com>
2016-10-30 16:22:54 +0200

    annotations appear synced up vith the video

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -30,8 +30,8 @@ let Video = {
     })
 
     vidChannel.join()
-      .receive("ok", ({annotations}) => {
-        annotations.forEach(ann => this.renderAnnotation(msgContainer, ann))
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", reason ))
   },
@@ -53,6 +53,25 @@ let Video = {
     `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations){
+    setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer){
+    return annotations.filter(ann => {
+      if (ann.at > seconds){
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
   },
   formatTime(at) {
     let date = new Date(null)
